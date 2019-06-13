43387929070be7c2ffc7025773dc967e92ed71ba
Anna Wicker <anna.wicker@sbcglobal.net>
2019-06-13 11:03:51 -0500

    added string practice labs

--- functions_practice.py
+++ functions_practice.py
@@ -18,3 +18,19 @@ x = int(input("Enter X"))
 y = int(input("Enter Y"))
 z = int(input("Enter Z"))
 print(avg_numbers(x, y, z,))
+
+
+def is_letter(character):
+    return character[0].lower() in "qwertyuiopasdfghjklzxcvbnm"
+
+print(is_letter("i"))
+print(is_letter("0"))
+
+def short_hand(short):
+    short = short.lower().replace("and", "&").replace("too", "2").replace("you", "U").replace("for", "4")
+    short = short.replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "")
+    return short
+
+phrase = input("Enter phrase:") #then enter "phrase" instead of phrase)
+print(short_hand("Thank you for that! You are too sweet and kind"))
+print(short_hand("How are you today?"))
--- string_practice.py
+++ string_practice.py
@@ -12,13 +12,23 @@ date: 2019-06-11 13:45
 
 #short hand
 
-short_hand = input("Enter a phrase")
-new_short_hand = short_hand.replace("and", "&").replace("too", "2").replace("you", "U").replace("You", "U").replace("for", "4")\
-    .replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "")
-print(new_short_hand)
+#short_hand = input("Enter a phrase")
+#new_short_hand = short_hand.replace("and", "&").replace("too", "2").replace("you", "U").replace("You", "U").replace("for", "4")\
+#    .replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "")
+#print(new_short_hand)
 
 #Remove Case and Punctuation
 
-#answer = input("Enter a phrase")
-#phrase =
+def phrase(punctuation):
+    punctuation = punctuation.lower().replace(",", "").replace("'", "").replace(" ", "")
+    return punctuation
 
+new_phrase= input("Enter phrase:")
+print(phrase(new_phrase))
+
+#Palindrome
+
+palindrome = input("Enter phrase:")
+palindrome = phrase(palindrome)
+backwards = palindrome[::-1]
+print(palindrome == backwards)
